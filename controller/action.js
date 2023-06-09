2bd095cab1ac6dfe31ac1199736bcbad85720c24
KaDaWaa <segev1278@gmail.com>
2023-06-09 20:04:16 +0300

    small update

--- controller/action.js
+++ controller/action.js
@@ -40,12 +40,19 @@ router.get('/dashboard', async (req, res) => {
   const username = req.query.username;
   const password = req.query.password;
   if (username == process.env.ADMIN_USERNAME && password == process.env.ADMIN_PASSWORD) {
+    Movie.findAll()
+    .then(movies=>{
     res.render('dashboard', {
-      pageTitle: 'Dashboard'
-    });
-  } else
-    res.redirect('/homepage');
-
+      pageTitle: 'Dashboard',
+      movies:movies
+    })
+  })
+  .catch(error=>{
+         res.render('homepage',{
+           pageTitle:'TCinemaB',
+         })
+       })
+}
 })
 
 // router.get('/dashboard',async(req,res)=>{
